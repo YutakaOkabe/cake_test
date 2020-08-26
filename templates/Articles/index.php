12cb1ebc48cc442d39b1bb277dc9323aec4f1625
YutakaOkabe <okabe.yutaka.833@gmail.com>
2020-08-26 16:24:21 +0900

    インデントの変更

--- templates/Articles/index.php
+++ templates/Articles/index.php
@@ -1,15 +1,17 @@
+<!-- File: templates/Articles/index.php  (削除リンク付き) -->
+
 <h1>記事一覧</h1>
-
-<p><?= $this->Html->link('記事の追加', ['action' => 'add']) ?></p>
-
+<p><?= $this->Html->link("記事の追加", ['action' => 'add']) ?></p>
 <table>
     <tr>
         <th>タイトル</th>
         <th>作成日時</th>
+        <th>操作</th>
     </tr>
 
-    <!-- ここで、$articles クエリーオブジェクトを繰り返して、記事の情報を出力します -->
-    <?php foreach ($articles as $article): ?>
+<!-- ここで、$articles クエリーオブジェクトを繰り返して、記事情報を出力します -->
+
+<?php foreach ($articles as $article): ?>
     <tr>
         <td>
             <?= $this->Html->link($article->title, ['action' => 'view', $article->slug]) ?>
@@ -26,5 +28,6 @@
             ?>
         </td>
     </tr>
-    <?php endforeach; ?>
+<?php endforeach; ?>
+
 </table>
