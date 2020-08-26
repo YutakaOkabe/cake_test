9ba2c28d9de1c40a31d1bd8fb56867b555845ccb
YutakaOkabe <okabe.yutaka.833@gmail.com>
2020-08-26 14:31:16 +0900

    バリデーションを追加

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -3,12 +3,41 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+// Text クラス
+use Cake\Utility\Text;
+// EventInterface クラス
+use Cake\Event\EventInterface;
+
+use Cake\Validation\Validator;
+
 
 class ArticlesTable extends Table
 {
-    public function initialize(array $config) : void
+    public function initialize(array $config): void
     {
         // created や modified カラムを自動的に更新
         $this->addBehavior('Timestamp');
+    }
+
+    public function beforeSave(EventInterface $event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            // スラグをスキーマで定義されている最大長に調整
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
+    }
+
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->notEmptyString('title')
+            ->minLength('title', 10)
+            ->maxLength('title', 255)
+
+            ->notEmptyString('body')
+            ->minLength('body', 10);
+
+        return $validator;
     }
 }
