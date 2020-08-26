cec41e7d894bbdfa5d58f8e618ac64bd8974c000
YutakaOkabe <okabe.yutaka.833@gmail.com>
2020-08-26 14:57:37 +0900

    tagsのMVCを作成

---
+++ tests/Fixture/TagsFixture.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * TagsFixture
+ */
+class TagsFixture extends TestFixture
+{
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // phpcs:disable
+    public $fields = [
+        'id' => ['type' => 'integer', 'length' => 10, 'autoIncrement' => true, 'default' => null, 'null' => false, 'comment' => null, 'precision' => null, 'unsigned' => null],
+        'title' => ['type' => 'string', 'length' => 191, 'default' => null, 'null' => true, 'collate' => null, 'comment' => null, 'precision' => null],
+        'created' => ['type' => 'timestampfractional', 'length' => null, 'default' => null, 'null' => true, 'comment' => null, 'precision' => 6],
+        'modified' => ['type' => 'timestampfractional', 'length' => null, 'default' => null, 'null' => true, 'comment' => null, 'precision' => 6],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+            'tags_title_key' => ['type' => 'unique', 'columns' => ['title'], 'length' => []],
+        ],
+    ];
+    // phpcs:enable
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'title' => 'Lorem ipsum dolor sit amet',
+                'created' => 1598420315,
+                'modified' => 1598420315,
+            ],
+        ];
+        parent::init();
+    }
+}
