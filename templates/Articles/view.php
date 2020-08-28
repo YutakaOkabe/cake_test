769f7af4112e983bf565a65f8ac54a83e1f4dd93
YutakaOkabe <okabe.yutaka.833@gmail.com>
2020-08-28 17:00:30 +0900

    あとでUserとアソシエーションを張る

--- templates/Articles/view.php
+++ templates/Articles/view.php
@@ -3,4 +3,6 @@
 <h1><?= h($article->title) ?></h1>
 <p><?= h($article->body) ?></p>
 <p><small>作成日時: <?= $article->created->format(DATE_RFC850) ?></small></p>
+<p><small>投稿者: <?= $article->user->format(DATE_RFC850) ?></small></p>
+
 <p><?= $this->Html->link('編集', ['action' => 'edit', $article->slug]) ?></p>
