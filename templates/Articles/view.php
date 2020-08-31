bad1790a9d79060c6fc0ae78b68700ea1d8aa522
YutakaOkabe <okabe.yutaka.833@gmail.com>
2020-08-31 16:46:17 +0900

    各セクションの詳細ページを色々変更

--- templates/Articles/view.php
+++ templates/Articles/view.php
@@ -1,8 +1,14 @@
 <!-- File: templates/Articles/view.php -->
-
 <h1><?= h($article->title) ?></h1>
 <p><?= h($article->body) ?></p>
+
 <p><small>作成日時: <?= $article->created->format(DATE_RFC850) ?></small></p>
-<p><small>投稿者: <?= $article->user->format(DATE_RFC850) ?></small></p>
+<p><small>タグ:
+        <?php $tags = $article->tags ?>
+        <?php foreach ($tags as $tag) : ?>
+            <?php echo h($tag->title); ?>
+        <?php endforeach; ?>
+    </small></p>
+<p><small>投稿者:<?php echo h($article->user->email) ?></small></p>
 
 <p><?= $this->Html->link('編集', ['action' => 'edit', $article->slug]) ?></p>
